2a7cfe59a763d9a70a2121c46b1d7723330f7745
Prithvi4519 <prithvik1019@gmail.com>
2025-03-15 17:53:06 +0530

    Configuered database for Render

--- foodista/settings.py
+++ foodista/settings.py
@@ -75,14 +75,17 @@ WSGI_APPLICATION = 'foodista.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+import dj_database_url
+import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+'default': dj_database_url.config(default=os.getenv('postgresql://postgree_cnq0_user:k6jtJsSFRucuZdIAQoubkHGRAqcwnb4L@dpg-cvamupnnoe9s73fc5t3g-a/postgree_cnq0'))
 
+}
+ALLOWED_HOSTS = ['foodista.onrender.com', 'localhost']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
